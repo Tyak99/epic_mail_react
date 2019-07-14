92a7ef799df0c07de141ce49da896d70115046e3
Tunde Yakub <tundenasri@gmail.com>
2019-07-14 17:01:14 +0100

    setup travis

--- src/__test__/App.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-
-describe('component: App', () => {
-  it('renders correctly', () => {
-    shallow(<App />);
-  });
-});
