31943100b3ca4c0b77439b0f66bcdcf0ff748a53
Tunde Yakub <tundenasri@gmail.com>
2019-07-19 20:18:29 +0100

    create sidebar component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+  faInbox,
+  faAngleDoubleUp,
+  faBox,
+  faThLarge,
+  faPlus,
+  faPaperPlane,
+} from '@fortawesome/free-solid-svg-icons';
 import App from './App';
 import store from '../store/index';
-import 'bootstrap/dist/css/bootstrap.min.css?raw';
+// import 'bootstrap/dist/css/bootstrap.min.css?raw';
+
+library.add(faInbox, faAngleDoubleUp, faBox, faThLarge, faPlus, faPaperPlane);
 
 render(
   <Provider store={store}>
